44948d41b498500903f89142e2abe99eb24f9ee8
Ashfordcliff <lashfordafful@gmail.com>
2022-12-22 03:17:54 -0800

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,9 +14,9 @@ char *leet(char *str)
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
-		if (str[index1] == leet[index2] ||
-		str[index1] - 32 == leet[index2])
-			str[index1] = index2 + '0';
+			if (str[index1] == leet[index2] ||
+		   	str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 		}
 	}
 	return (str);
