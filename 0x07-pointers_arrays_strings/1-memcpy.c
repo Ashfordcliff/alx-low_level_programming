2ad0222ebf3487d19f76eaebf5e51eb8d6195c5b
Ashfordcliff <lashfordafful@gmail.com>
2023-01-03 09:23:19 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: lengh of buffer
+ * Return: new string.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned ont i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
