80860677c2309cddb33500dd8bf0ac55c2704f13
Ashfordcliff <lashfordafful@gmail.com>
2023-01-09 02:16:07 -0800

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the lenght of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Lennght of a string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index  = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
+}
