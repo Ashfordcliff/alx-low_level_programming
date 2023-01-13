a8b5aa9a934bc8e9722683877ba9509c5acdfb45
Ashfordcliff <lashfordafful@gmail.com>
2023-01-12 21:23:08 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,8 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	if (num >= _strlen(s2) /* account for n too big */
-			num = _strlen(s2);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
 	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
